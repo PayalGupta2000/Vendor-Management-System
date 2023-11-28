0f99e919e150976d1ea1c2ec72b7c4bcd96c29d3
ujjwall7 <sharmaujjwal0921@gmail.com>
2023-11-28 21:22:45 +0530

    fixed bugs

--- vendors/urls.py
+++ vendors/urls.py
@@ -2,9 +2,7 @@ from django.urls import path
 from . views import *
 
 urlpatterns = [
-    path('vendors/',VenorListView.as_view(),name='vendor-list'),
-    path('vendors/<int:vendor_id>/',VendorDetailView.as_view(),name='vendor-detail'),
-    path('purchase_orders/',PurchaseOrderListView.as_view(),name='purchase-order-list'),
-    path('purchase_orders/<int:po_id>/',PurchaseOrderListView.as_view(),name='purchase-order-detail'),
-    path('vendors/<int:vendor_id>/performance/',vendorPerformanceView.as_view(),name='vendor-performance'),
+    path('vendors/',VenorAPIView.as_view(),name='vendor-list'),
+    path('purchase_orders/',PurchaseOrderAPIView.as_view(),name='purchase-order-list'),
+    path('vendors/',vendorPerformanceAPIView.as_view(),name='vendor-performance'),
 ]
